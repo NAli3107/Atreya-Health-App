adf478101177ca1628af383ede068f8bee95b8db
farhahdin1997 <farhahdin97@hotmail.com>
2022-06-03 19:24:26 +0100

    set up mongo db

--- server/index.js
+++ server/index.js
@@ -4,3 +4,16 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+const app = express ();
+app.use (bodyParser.json({ limit: "300mb", extended:true}));
+app.use (bodyParser.urlencoded({ limit: "300mb", extended:true}));
+app.use(cors());  
+
+const CONNECTION_URL = 'mongodb+srv://Atreya2022:Atreya2022@cluster0.jdjuv.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true , useUnifiedTopology : true})
+.then (()=> app.listen(PORT,() => console.log(`Server running on port: ${PORT} `)))
+.catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify' ,false)
