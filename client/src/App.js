61b19634b5c6b547a02f031aaade37121b60f298
Humma Noshine <Hummanoshine@gmail.com>
2022-06-06 15:28:14 +0100

    doesnt give me the login form

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+
+import { setContext } from "@apollo/client/link/context";
 import './App.css';
 import LoginForm from "./components/form/LoginForm";
-import Test from "./components/form/testhig"
+// import Test from "./components/form/testhig"
 
 
 function App() {
   return (
     <div className="App">
       Hi all - whys this not wokring
-<Test/>
-      <LoginForm/>
+<LoginForm/>
     </div>
   );
 }
