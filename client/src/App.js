9ccfa39fe0c78f92d10a8e4f2231e82ae340e69e
Humma Noshine <Hummanoshine@gmail.com>
2022-06-07 20:21:42 +0100

    trying

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import Login from "./components/form/LoginForm"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello Doctors</h1>
+      <Login/>
+
     </div>
   );
 }
---
+++ client/src/components/form/LoginForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Form} from 'react-bootstrap';
+
+
+function login () {
+    return(
+    <div>smells
+      dum shit
+
+
+</div>
+    )
+}
+
+export default login; 
