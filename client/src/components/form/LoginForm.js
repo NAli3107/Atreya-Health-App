cb71e8eecdb261fc7699ba213840d364b6d9c9df
Humma Noshine <Hummanoshine@gmail.com>
2022-06-08 12:30:11 +0100

    some errors

--- client/src/components/form/LoginForm.js
+++ client/src/components/form/LoginForm.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import { Form, Button, Alert } from "react-bootstrap";
 import Auth from "../../utils/auth";
 import { useMutation } from "@apollo/client";
-import { LOGIN_USER } from "../../utils/mutations";
+import {LOGIN_USER} from "../../utils/mutations"
 
 const LoginForm = () => {
     const [userFormData, setUserFormData] = useState({ email: "", password: "" });
@@ -28,6 +28,8 @@ const LoginForm = () => {
           event.preventDefault();
           event.stopPropagation();
         }
+
+        // LOGIN PART NOT WORKING THANKS TO MUTATIONS
         try {
             const { data } = await login({
               variables: { ...userFormData },
@@ -39,7 +41,6 @@ const LoginForm = () => {
         }
     
         setUserFormData({
-          username: "",
           email: "",
           password: "",
         });
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,37 +1,25 @@
 import { gql } from '@apollo/client';
 // hn
-// export const LOGIN_USER = gql`
-//   mutation login($email: String!, $password: String!) {
-//     login(email: $email, password: $password) {
-//           _id
-//           email
-//           password
-//     }
-//   }
-// `;
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
-      user {
-        _id
-        username
+      user{
+          email
+          password
       }
     }
   }
 `;
-
-`;
-
-// export const ADD_USER = gql`
-//   mutation addUser($username: String!, $email: String!, $password: String!) {
-//     addUser(username: $username, email: $email, password: $password) {
-//  token
-//  user{
-//      _id
-//      username
-//      email
-//  }
+// export const LOGIN_USER = gql`
+//   mutation login($email: String!, $password: String!) {
+//     login(email: $email, password: $password) {
+//       token
+//       user {
+//         _id
+//         username
+//       }
 //     }
 //   }
 // `;
+
