00197c675bfb894a19a800053e5bd64cbefaef5e
Humma Noshine <Hummanoshine@gmail.com>
2022-06-08 17:24:40 +0100

    added appolo provider in appjs

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import Login from "./components/form/LoginForm"
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import './App.css';
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
+
 
 function App() {
   return (
+
+    <ApolloProvider client={client}>
     <div className="App">
       <h1>Hello Doctors</h1>
       <Login/>
 
     </div>
+    </ApolloProvider>
   );
 }
 export default App;
