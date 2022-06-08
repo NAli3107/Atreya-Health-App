eeac82d3df4c443f0db7cf0d9627d20250aa087e
Humma Noshine <Hummanoshine@gmail.com>
2022-06-08 14:52:48 +0100

    added models, schemas and auth.js in server

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,9 +1,35 @@
-const { gql } = require("apollo-server-express");
+// const { gql } = require("apollo-server-express");
+
+// const typeDefs = gql`
+// type Query {
+//     dummy: String
+//   }
+// `;
+
+// module.exports = typeDefs;
+
+
+const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+type User {
+  _id: ID!
+  email: String!
+  password: String!
+}
+type Auth {
+  token: ID!
+  user: User
+}
 type Query {
-    dummy: String
-  }
+  me: User
+}
+type Mutation {
+  login(email: String!, password: String!): Auth
+  addUser(username: String!, email: String!, password:String!): Auth
+}
 `;
 
 module.exports = typeDefs;
+
+
