010edc5f4a2b80f8e4c863cdcb50ca23e0c986ea
Humma Noshine <Hummanoshine@gmail.com>
2022-06-08 12:35:27 +0100

    still the same

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,15 +1,13 @@
 import { gql } from '@apollo/client';
 // hn
 export const LOGIN_USER = gql`
-  mutation login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
-      token
-      user{
-          email
-          password
-      }
-    }
+mutation login($email: String!, $password: String!) {
+  login(email: $email, password: $password) {
+        _id
+        email
+        password
   }
+}
 `;
 // export const LOGIN_USER = gql`
 //   mutation login($email: String!, $password: String!) {
