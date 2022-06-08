d9e02a441a9a2712ce96ef88781dfb08da6f3757
Humma Noshine <Hummanoshine@gmail.com>
2022-06-08 12:19:37 +0100

    mutations giving me grief

--- client/src/components/form/LoginForm.js
+++ client/src/components/form/LoginForm.js
@@ -1,70 +1,61 @@
 // see SignupForm.js for comments
 import React, { useState } from "react";
 import { Form, Button, Alert } from "react-bootstrap";
-// import Auth from "../../utils/auth";
-// import { useMutation } from "@apollo/client";
-// import { LOGIN_USER } from "../../utils/mutations";
+import Auth from "../../utils/auth";
+import { useMutation } from "@apollo/client";
+import { LOGIN_USER } from "../../utils/mutations";
 
-// const LoginForm = () => {
-//   const [userFormData, setUserFormData] = useState({ email: "", password: "" });
-//   const [validated] = useState(false);
-//   const [showAlert, setShowAlert] = useState(false);
-
-//   const [login, { error }] = useMutation(LOGIN_USER);
-
-//   const handleInputChange = (event) => {
-//     const { name, value } = event.target;
-//     setUserFormData({ ...userFormData, [name]: value });
-//   };
-
-//   const handleFormSubmit = async (event) => {
-//     event.preventDefault();
-
-//     // check if form has everything (as per react-bootstrap docs)
-//     const form = event.currentTarget;
-//     if (form.checkValidity() === false) {
-//       event.preventDefault();
-//       event.stopPropagation();
-//     }
-
-//     try {
-//       const { data } = await login({
-//         variables: { ...userFormData },
-//       });
-
-//       Auth.login(data.login.token);
-//     } catch (error) {
-//       console.error(error);
-//     }
-
-//     setUserFormData({
-//       username: "",
-//       email: "",
-//       password: "",
-//     });
-//   };
 const LoginForm = () => {
     const [userFormData, setUserFormData] = useState({ email: "", password: "" });
+    const [validated] = useState(false);
+    const [showAlert, setShowAlert] = useState(false);
+ const [login, { error }] = useMutation(LOGIN_USER);
+
+
+    // allow user to input
     const handleInputChange = (event) => {
         const { name, value } = event.target;
         setUserFormData({ ...userFormData, [name]: value });
       };
 
-      const handleFormSubmit = async (event) => {
+    //   button
+    const handleFormSubmit = async (event) => {
         event.preventDefault();
-      console.log("something clicked")}
+    console.log("button clicked")
+        // check if form has everything (as per react-bootstrap docs)
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        try {
+            const { data } = await login({
+              variables: { ...userFormData },
+            });
+
+            Auth.login(data.login.token);
+        } catch (error) {
+          console.error(error);
+        }
+    
+        setUserFormData({
+          username: "",
+          email: "",
+          password: "",
+        });
+      };
 
   return (
     <>
-      <Form onSubmit={handleFormSubmit} >
-        {/* <Alert
+      <Form noValidate validated={validated} onSubmit={handleFormSubmit} >
+        <Alert
           dismissible
           onClose={() => setShowAlert(false)}
           show={showAlert}
           variant="danger"
         >
           Something went wrong with your login credentials!
-        </Alert> */}
+        </Alert>
         <Form.Group>
           <Form.Label htmlFor="email">Email</Form.Label>
           <Form.Control
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,13 +1,26 @@
 import { gql } from '@apollo/client';
 // hn
+// export const LOGIN_USER = gql`
+//   mutation login($email: String!, $password: String!) {
+//     login(email: $email, password: $password) {
+//           _id
+//           email
+//           password
+//     }
+//   }
+// `;
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
-          _id
-          email
-          password
+      token
+      user {
+        _id
+        username
+      }
     }
   }
+`;
+
 `;
 
 // export const ADD_USER = gql`
