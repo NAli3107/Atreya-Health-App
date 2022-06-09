946231fbe1d56d909b48bd8994adb7e15462989d
Sumaya Gure <sumayabile8@gmail.com>
2022-06-10 00:59:01 +0100

    css

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Navbar from'./components/landingpage/Navbar';
 import "./app.css"
-import Humma from "./images/yoga.jpg"
+import yoga from "./images/yoga.jpg"
 
 function App() {
   return (
     <div>
       <Navbar />
-      <p>I believe that the greatest gift you can give your family and the world 
-        is a healthy you.</p>
-        <img src={Humma}></img>
+      <p>"I believe that the greatest gift you can give your family and the world 
+        is a healthy you"</p>
+        <img src={yoga}></img>
     </div>
   
     );
