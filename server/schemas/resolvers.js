e57a7187072ad08c451be10f1bda18e7b226e94d
Nadira Ali <nadiraali188a@gmail.com>
2022-06-11 11:20:00 +0100

    crud

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,20 +1,20 @@
-const { User, Posts } = require('../models');
-const { AuthenticationError} = require ("apollo-server-express");
-const {signToken} = require("../utils/auth");
+const { User, Posts } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
-   me: async (parent, args, context) => {
+    me: async (parent, args, context) => {
       if (context.user) {
         const userData = User.findOne({ _id: context.user._id }).select(
           "-__v-password"
         );
-        console.log(userData, "from query in resolvers")
+        console.log(userData, "from query in resolvers");
         return userData;
       }
       throw new AuthenticationError("You need to be logged in!");
+    },
   },
-},
   Mutation: {
     addUser: async (parent, args) => {
       const user = await User.create(args);
@@ -35,16 +35,20 @@ const resolvers = {
       }
 
       const token = signToken(user);
-      console.log('from login', user);
+      console.log("from login", user);
       return { token, user };
     },
     createPost: async (parent, { postInfo }, context) => {
-      console.log(postInfo, context.user,);
       if (context.user) {
         const newPosts = await Posts.create({
-          postInfo, 
-          creator: context.user.username
-      });
+          postInfo,
+          creator: context.user.username,
+        });
+
+        await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { posts: newPosts._id } }
+        );
 
         return newPosts;
       }
@@ -80,7 +84,7 @@ const resolvers = {
     //     return likePost;
     //   }
     // },
-  },  
+  },
 };
 
 module.exports = resolvers;
