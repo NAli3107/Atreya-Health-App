00f3a8e0f92643c4cf45cebf5659eb6327a184c5
Nadira Ali <nadiraali188a@gmail.com>
2022-06-11 12:47:49 +0100

    updated changes

--- server/models/User.js
+++ server/models/User.js
@@ -18,6 +18,12 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
+    posts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Posts',
+      },
+    ],
   },
   // set this to use virtual below
   {
