e303e98134268e37e4040d95d51cd9a8922d4fdf
Nadira Ali <nadiraali188a@gmail.com>
2022-06-15 23:22:26 +0100

    handleFormSubmit function

--- client/src/components/postForm/NewPost.js
+++ client/src/components/postForm/NewPost.js
@@ -1,62 +1,93 @@
 import React from "react";
-
-import WallForm from "./Wall"
-
-// import { createPost } from "../services/firebase";
-// import { authentication } from "../services/firebase-config";
-
-import './Newpost.css';
-import {CREATE_POST} from "../../utils/mutations"
+import Auth from "../../utils/auth";
+import WallForm from "./Wall";
+import "./Newpost.css";
+import { CREATE_POST } from "../../utils/mutations";
 // import anonymous from '../images/default.png';
-
 
 // function handleSubmission(e) {
 //     e.preventDefault();
 //     createPost('test123');
 // }
 
-function magic(input) {
-    input = input.replace(/&/g, '&amp;');
-    input = input.replace(/</g, '&lt;');
-    input = input.replace(/>/g, '&gt;');
-    return input;
-}
-
-const handleUpdate = async (event) => {
-    event.preventDefault();
-console.log("button clicked")
+// function magic(input) {
+//   input = input.replace(/&/g, "&amp;");
+//   input = input.replace(/</g, "&lt;");
+//   input = input.replace(/>/g, "&gt;");
+//   return input;
+// }
 
 //take the title message etc and save to db  - new post.js
 //qurey - all the posts on the wall and retreving db - in wall.js
 
-const onUpdate = ({
-   
-  });
+const NewPost = () => {
+  const [userFormData, setUserFormData] = useState({ title: "", message: "" });
+  const [validated] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
 
-   const addPost= ({
+  const [post, { error }] = useMutation(CREATE_POST);
 
-   })
-}
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUserFormData({ ...userFormData, [name]: value });
+  };
 
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
 
-    const NewPost = () => {
+    // check if form has everything (as per react-bootstrap docs)
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
 
-        return(
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+    console.log(token);
+    if (!token) {
+      return false;
+    }
+    try {
+      const { data } = await post({
+        variables: { ...userFormData },
+      });
+    } catch (error) {
+      console.error(error);
+    }
+    setUserFormData({
+      title: "",
+      message: "",
+    });
+  };
+
+  return (
     <div>
-
-     <div id="newpost">
-        <h1>What is on your <span>mind</span>?</h1>
-    <form onSubmit={handleUpdate}>
-            <textarea id="post-title" className="form-primary" placeholder="Title your post!"></textarea>
-            <textarea id="post-msg" className="form-secondary" placeholder="Then tell everyone what you're thinking..."></textarea>
-            <button type="submit">Post to your Public Wall!</button>
+      <div id="newpost">
+        <h1>
+          What is on your <span>mind</span>?
+        </h1>
+        <form onSubmit={handleFormSubmit}>
+          <textarea
+            id="post-title"
+            className="form-primary"
+            placeholder="Title your post!"
+            name="title"
+            onChange={handleInputChange}
+            value={userFormData.title}
+            required
+          ></textarea>
+          <textarea
+            id="post-msg"
+            className="form-secondary"
+            placeholder="Then tell everyone what you're thinking..."
+          ></textarea>
+          <button type="submit">Post to your Public Wall!</button>
         </form>
+      </div>
+
+      <WallForm />
     </div>
-   
+  );
+};
 
-    <WallForm/>
-    </div>
-)}
-
-export default NewPost
-
+export default NewPost;
