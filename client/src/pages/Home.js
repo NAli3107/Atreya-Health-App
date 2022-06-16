c2925f28a1e00bdcf44f867ec183eb2b8636d0c2
Sumaya Gure <sumayabile8@gmail.com>
2022-06-16 19:48:25 +0100

    gg

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,17 +1,17 @@
-import React from "react";
-// import Post from './Post.js';
-import '../components/postForm/Wall';
+// import React from "react";
+// // import Post from './Post.js';
+// import '../components/postForm/Wall';
 
-const Wall = ({ posts }) => {
-    return <div id="wall">
-      <h1>HOME</h1>
-        <h1>Public Wall</h1>
-        <h2>You can view the recent posts made by members of our community.</h2>
+// const Wall = ({ posts }) => {
+//     return <div id="wall">
+//       <h1>HOME</h1>
+//         <h1>Public Wall</h1>
+//         <h2>You can view the recent posts made by members of our community.</h2>
 
-        <div id="post-list">TEST
-            {/* {posts.slice(0, 5).map(p => <Post key={p.postId} props={p} />)} */}
-        </div>
-    </div>
-}
+//         <div id="post-list">TEST
+//             {/* {posts.slice(0, 5).map(p => <Post key={p.postId} props={p} />)} */}
+//         </div>
+//     </div>
+// }
 
-export default Wall;
+// export default Wall;
