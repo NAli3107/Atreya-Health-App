109ce9fd551e7cfb1b27d9a779ef88900b79f267
Humma Noshine <Hummanoshine@gmail.com>
2022-06-17 08:59:24 +0100

    csing

--- client/src/components/postForm/Wall.js
+++ client/src/components/postForm/Wall.js
@@ -3,7 +3,10 @@ import React from "react";
 import "./Wall.css";
 import { QUERY_POSTS } from "../../utils/queries";
 // import { useEffect } from "react";
-
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
 
 const Wall = () => {
   const { loading, data } = useQuery(QUERY_POSTS);
@@ -19,16 +22,39 @@ const Wall = () => {
       <h2>You can view the recent posts made by members of our community.</h2>
 
       <div id="post-list">
+
         {posts.map((post) => (
-          <div className="card">
-            <div className="card-body">
+
+            <div className=" cardbox card">
+
+
+{/*          
+           <div className="card-body"> */}
+           <div class="thumbnail1 card-body">
               <h3 className="card-title">{post.title}</h3>
-              <h5 className="card-subtitle">{post.creator}</h5>
+              <div className="pbody">
               <p className="card-text">{post.message}</p>
+              </div>
+              <h5 className="card-subtitle">{post.creator}</h5>
             </div>
-          </div>
+            </div>
+
+  // </div>
+//   <Card style={{ width: '18rem' }}>
+//   <Card.Img variant="top" src="holder.js/100px180" />
+//   <Card.Body>
+//     <Card.Title>{post.title}</Card.Title>
+//     <Card.Text>
+//     {post.message}
+//     </Card.Text>
+//   </Card.Body>
+// </Card>
+
+     
         ))}
       </div>
+
+       
     </div>
   );
 };
