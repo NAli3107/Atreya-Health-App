05699460550dbca57b1a35c1b1b5c20bf690c737
Humma Noshine <Hummanoshine@gmail.com>
2022-06-17 17:34:49 +0100

    footer and scrolling

---
+++ client/src/components/landingpage/footer.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import "./Footer.css"
+
+function Footer() {
+    return <div id="footer">
+    < div className="footerWrapper">
+    
+    <div bg='dark' variant='dark' expand='lg'>
+
+    😎<li className="navbarIconBadge"><a href="https://github.com/HummaNosh?tab=repositories"  target='_blank'> Humma</a></li>     
+      <li className="navbarIconBadge"><a href="https://github.com/NAli3107"  target='_blank'> Nadira</a></li>
+      <li className="navbarIconBadge"><a href="https://github.com/farhahdin1997"  target='_blank'> Farha</a></li>
+      <li className="navbarIconBadge"><a href="https://github.com/SuM949"  target='_blank'> Sumaya</a></li>  
+      <li className="navbarIconBadge"><a href="https://github.com/AbriCS"  target='_blank'> Jean</a></li> 
+      <li className="navbarIconBadge"><a href="https://github.com/HummaNosh?tab=repositories"  target='_blank'> Jibril</a></li> 
+      <li className="navbarIconBadge"> <a href="https://github.com/AkashWanem"  target='_blank'>Akash</a></li> 😎
+      </div> 
+        </div> 
+        </div>
+  
+}
+export default Footer;
