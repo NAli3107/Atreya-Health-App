2862fdd6ffed3e9e453fa84af5946cb661c6eac3
Humma Noshine <Hummanoshine@gmail.com>
2022-06-17 09:51:51 +0100

    added date

--- client/src/components/postForm/Wall.js
+++ client/src/components/postForm/Wall.js
@@ -2,11 +2,9 @@ import { useQuery, gql } from "@apollo/client";
 import React from "react";
 import "./Wall.css";
 import { QUERY_POSTS } from "../../utils/queries";
-// import { useEffect } from "react";
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
-import Card from 'react-bootstrap/Card';
+
+import moment from 'moment'
+const timestamp = Date.now();
 
 const Wall = () => {
   const { loading, data } = useQuery(QUERY_POSTS);
@@ -18,37 +16,22 @@ const Wall = () => {
     }
   return (
     <div id="wall">
-      <h1>Public Wall</h1>
-      <h2>You can view the recent posts made by members of our community.</h2>
+      <h1>Your Community</h1>
+      <h2>Check out what the Community have to say here</h2>
 
       <div id="post-list">
-
         {posts.map((post) => (
-
             <div className=" cardbox card">
-
-
-{/*          
-           <div className="card-body"> */}
            <div class="thumbnail1 card-body">
               <h3 className="card-title">{post.title}</h3>
               <div className="pbody">
               <p className="card-text">{post.message}</p>
               </div>
-              <h5 className="card-subtitle">{post.creator}</h5>
+              <h5 className="card-subtitle">Created by: {post.creator} on {moment({timestamp}).format('MMMM Do YYYY')}</h5>
+            {/* <button className="edit">Edit</button>
+            <button className="edit">Delete</button> */}
             </div>
             </div>
-
-  // </div>
-//   <Card style={{ width: '18rem' }}>
-//   <Card.Img variant="top" src="holder.js/100px180" />
-//   <Card.Body>
-//     <Card.Title>{post.title}</Card.Title>
-//     <Card.Text>
-//     {post.message}
-//     </Card.Text>
-//   </Card.Body>
-// </Card>
 
      
         ))}
